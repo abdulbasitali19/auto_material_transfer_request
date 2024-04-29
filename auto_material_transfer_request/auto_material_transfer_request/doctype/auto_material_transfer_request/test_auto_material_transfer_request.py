19e48d5aed605c8049e62c646acfb6709d3193f7
abdulbasitali19 <abdulbasitalishaik1@gmail.com>
2024-04-29 08:27:03 +0000

    automate material request

---
+++ auto_material_transfer_request/auto_material_transfer_request/doctype/auto_material_transfer_request/test_auto_material_transfer_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, abdul basit and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAutoMaterialTransferRequest(FrappeTestCase):
+	pass
